dbca4475aac1739d9c452d28efb510f6a689ef76
WGS-94 <wilson-senx@hotmail.com>
2024-09-26 21:44:19 -0300

    :fire: Exercitando primeiros passos no javascript

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,20 @@
+const constante = 2050
+
+let nome = prompt("Qual seu nome?")
+
+console.log("Seja bem vindo", nome)
+
+let nascimento = prompt("Qual ano você nasceu?")
+
+nascimento = Number(nascimento)
+
+console.log("Idade:", 2023 - nascimento)
+
+let ano = prompt("Em que ano estamos?")
+
+ano = Number(ano)
+
+console.log("É maior de idade?", ano >= nascimento)
+
+console.log("Idade em 2050:", constante - nascimento)
+
