7c4362b7afe671ebb47b0080a37ff59220b4269f
WGS-94 <wilson-senx@hotmail.com>
2024-10-03 20:01:59 -0300

    :tada: Exercício 1 da aula de strings

---
+++ JAVASCRIPT/aulas-9-10/index.js
@@ -0,0 +1,9 @@
+
+// Crie um programa que peça ao usuário para inserir o seu nome e sua cor favorita e imprima a mensagem "A cor do FULANO é COR"
+
+
+let nome = String(prompt("Qual seu nome?"));
+
+let cor = String(prompt("Qual a sua cor favorita"))
+
+console.log(`A  cor favorita do ${nome.toUpperCase()} é ${cor.toUpperCase()}`)
