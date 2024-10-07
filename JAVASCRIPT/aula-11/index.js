1aa74c8d28d5c396866c5f1666abc576bfea5849
WGS-94 <wilson-senx@hotmail.com>
2024-10-07 20:07:35 -0300

    :sparkles: Excercício 1 sobre funções

---
+++ JAVASCRIPT/aula-11/index.js
@@ -0,0 +1,16 @@
+
+// FUNÇÕES
+
+// Exercicio 1: Crie uma função que imprima no console a frase olá mundo
+
+function  imprimeOlaMundo() {
+    console.log("Olá mundo");
+}
+
+function imprimeOlaMundoTexto(texto) {
+    console.log(`${texto}`)
+}
+
+imprimeOlaMundo()
+
+imprimeOlaMundoTexto("Olá mundo, tudo bem?")
