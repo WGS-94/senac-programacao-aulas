6e6dd77b00ea1e7ec3e65058b0a62b1af9a3885a
WGS-94 <wilson-senx@hotmail.com>
2024-10-18 21:37:40 -0300

    :fire: Resolução do exercício 1 de escrita de código

--- JAVASCRIPT/aula-17-18/exercicio-escrita.js
+++ JAVASCRIPT/aula-17-18/exercicio-escrita.js
@@ -10,4 +10,38 @@
     b) Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, 
     e guarde esses nomes em um array
 
+    c) Por fim, imprima o array com os nomes dos bichinhos no console
+
 */
+
+// RESOLUÇÃO
+
+const controleBichinhos = () => {
+
+    const qtdeBichinhos = Number(prompt("Quantos bichinhos de estimação você tem ?"));
+
+    // a)
+    if(qtdeBichinhos === 0){
+        return `😔 Que pena! Você pode adotar um pet!` 
+    }
+
+    // b)
+    const nomesBichinhos = [];
+
+    for(let i = 0; i < qtdeBichinhos; i++){
+
+        const nomeBichinho = prompt(`Qual o nome do seu ${i+1}º`)
+
+        nomesBichinhos.push(nomeBichinho);
+    }
+
+    // c)
+    return nomesBichinhos;
+
+}
+
+const nomesBichinhos = controleBichinhos();
+console.log(JSON.stringify(nomesBichinhos))
+
+
+
