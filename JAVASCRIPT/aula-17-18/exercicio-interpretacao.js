19577cfe97889a40a42bef5f302bcebf02821bfb
WGS-94 <wilson-senx@hotmail.com>
2024-10-18 21:17:53 -0300

    :tada: Resolução exercío de interpretação

--- JAVASCRIPT/aula-17-18/exercicio-interpretacao.js
+++ JAVASCRIPT/aula-17-18/exercicio-interpretacao.js
@@ -4,8 +4,8 @@
 // EXERCICIO 1 - O que o código abaixo está fazendo? Qual o resultado impresso no console?
 
 let valor = 0
-for(let i = 0; i < 5; i++) {
-  valor += i
+for (let i = 0; i < 5; i++) {
+    valor += i
 }
 console.log(valor)
 
@@ -21,9 +21,9 @@ console.log(valor)
 
 const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
 for (let numero of lista) {
-  if (numero > 18) {
-		console.log(numero)
-	}
+    if (numero > 18) {
+        console.log(numero)
+    }
 }
 
 // a) O que vai ser impresso no console?
@@ -42,13 +42,13 @@ for (let numero of lista) {
 
 const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
 let quantidadeAtual = 0
-while(quantidadeAtual < quantidadeTotal){
-  let linha = ""
-  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
-    linha += "*"
-  }
-  console.log(linha)
-  quantidadeAtual++
+while (quantidadeAtual < quantidadeTotal) {
+    let linha = ""
+    for (let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++) {
+        linha += "*"
+    }
+    console.log(linha)
+    quantidadeAtual++
 }
 
 // RESPOSTA:  O resultado impresso no console seria: 4 linhas com asteriscos, cada uma com um asterisco a mais que a anterior.
